90ea3564b1427af684e9081142df3a24748070f2
Icosa-Dev <ljcottendev@gmail.com>
2026-02-13 16:27:32 -0500

    Refactored WeatherAPI

--- wsdesktop/src/main/java/io/github/icosadev/wsdesktop/WeatherAPI.java
+++ wsdesktop/src/main/java/io/github/icosadev/wsdesktop/WeatherAPI.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025 LJC
+ *
+ * SPDX-License-Identifier: GPL3-or-later
+ */
+
 package io.github.icosadev.wsdesktop;
 
 import java.io.IOException;
@@ -5,72 +11,60 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-/*
- * The WeatherAPI class is a singleton object
- * that returns the weather data of NYC from
- * the Open-Meteo weather API. Because the
- * weather API in Open-Meto requires coordinates
- * and not location names the object uses the
- * Open-Meteo geocoding API to get coordinates.
- * 
- * So the overall way the object works is
- * City Name -> Coordinates -> Weather JSON -> Printed String
+/* NOTE: For API specifications, see:
+ * https://open-meteo.com/en/docs#hourly_parameter_definition
  */
+
 public class WeatherAPI {
-    // Prints the weather at NYC
-    public static void getWeather() {
+    public static WeatherData getWeather(double longitude, double latitude) {
         try {
-            JSONObject cityLocationData = (JSONObject) getLocationData("NYC");
-            double latitude = (double) cityLocationData.get("latitude");
-            double longitude = (double) cityLocationData.get("longitude");
-            displayWeatherData(latitude, longitude);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Returns the location data from a city name
-    private static JSONObject getLocationData(String city) {
-        city = city.replaceAll(" ", "+");
-
-        String urlString = "https://geocoding-api.open-meteo.com/v1/search?name=" +
-                city + "&count=1&language=en&format=json";
-
-        try {
-            HttpURLConnection apiConnection = fetchApiResponse(urlString);
+            String url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude +
+                    "&longitude=" + longitude + "&current=temperature_2m,relative_humidity_2m,wind_speed_10m";
+            HttpURLConnection apiConnection = fetchApiResponse(url);
 
             if (apiConnection.getResponseCode() != 200) {
-                System.out.println("Error: Could not connect to API");
+                System.err.println("Error: Could not connect to API.");
                 return null;
             }
 
             String jsonResponse = readApiResponse(apiConnection);
 
             JSONParser parser = new JSONParser();
-            JSONObject resultsJsonObj = (JSONObject) parser.parse(jsonResponse);
+            JSONObject jsonObject = (JSONObject) parser.parse(jsonResponse);
+            JSONObject currentWeatherJson = (JSONObject) jsonObject.get("current");
 
-            JSONArray locationData = (JSONArray) resultsJsonObj.get("results");
-            return (JSONObject) locationData.get(0);
+            double temperature = (double) currentWeatherJson.get("temperature_2m");
 
+            long relativeHumidity = (long) currentWeatherJson.get("relative_humidity_2m");
+
+            double pressure = (double) currentWeatherJson.get("pressure_msl_surface_pressure");
+
+            double windSpeed = (double) currentWeatherJson.get("wind_speed_10m");
+
+            double windDirection = (double) currentWeatherJson.get("wind_direction_10m");
+
+            return new WeatherData(
+                    temperature,
+                    relativeHumidity,
+                    pressure,
+                    windSpeed,
+                    windDirection);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return null;
     }
 
-    // Get connection to API from URL
     private static HttpURLConnection fetchApiResponse(String urlString) {
         try {
-            // attempt to create connection
             @SuppressWarnings("deprecation")
             URL url = new URL(urlString);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-            // set request method to get
             conn.setRequestMethod("GET");
 
             return conn;
@@ -81,7 +75,6 @@ public class WeatherAPI {
         return null;
     }
 
-    // Reads from a connection to the API
     private static String readApiResponse(HttpURLConnection apiConnection) {
         try {
             StringBuilder resultJson = new StringBuilder();
@@ -95,45 +88,10 @@ public class WeatherAPI {
             scanner.close();
 
             return resultJson.toString();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         return null;
-    }
-
-    // Prints the weather data
-    private static void displayWeatherData(double latitude, double longitude) {
-        try {
-            String url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude +
-                    "&longitude=" + longitude + "&current=temperature_2m,relative_humidity_2m,wind_speed_10m";
-            HttpURLConnection apiConnection = fetchApiResponse(url);
-
-            if (apiConnection.getResponseCode() != 200) {
-                System.out.println("Error: Could not connect to API");
-                return;
-            }
-
-            String jsonResponse = readApiResponse(apiConnection);
-
-            JSONParser parser = new JSONParser();
-            JSONObject jsonObject = (JSONObject) parser.parse(jsonResponse);
-            JSONObject currentWeatherJson = (JSONObject) jsonObject.get("current");
-
-            String time = (String) currentWeatherJson.get("time");
-            System.out.println("Current Time: " + time);
-
-            double temperature = (double) currentWeatherJson.get("temperature_2m");
-            System.out.println("Current Temperature (C): " + temperature);
-
-            long relativeHumidity = (long) currentWeatherJson.get("relative_humidity_2m");
-            System.out.println("Relative Humidity: " + relativeHumidity);
-
-            double windSpeed = (double) currentWeatherJson.get("wind_speed_10m");
-            System.out.println("Weather Description: " + windSpeed);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
