2e14e9ee9ca8ab3734e840dbc5a628a0648eefb5
Icosa-Dev <ljcottendev@gmail.com>
2026-02-13 19:47:27 -0500

    Updated Main scene Controller class

--- wsdesktop/src/main/java/io/github/icosadev/wsdesktop/App.java
+++ wsdesktop/src/main/java/io/github/icosadev/wsdesktop/App.java
@@ -6,82 +6,23 @@
 
 package io.github.icosadev.wsdesktop;
 
-import io.github.icosadev.wsdesktop.api.Coordinates;
-import io.github.icosadev.wsdesktop.api.GeocodingAPI;
-import io.github.icosadev.wsdesktop.api.WeatherAPI;
-import io.github.icosadev.wsdesktop.api.WeatherData;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class App extends Application {
-    private static String city;
-    private static Timeline timeline;
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/Main.fxml"));
 
-    @FXML
-    private static Label titleLabel;
-    private static Text temperatureText;
-    private static Text humidityText;
-    private static Text pressureText;
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Running WSDesktop...");
         launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) {
-        try {
-            // Class variables
-            if (city == null) {
-                city = "NYC"; // Default to NYC
-            }
-
-            // Initialize scene and stage
-            Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-
-            // Update loop logic
-            update();
-
-            timeline = new Timeline(
-                new KeyFrame(Duration.minutes(1), e -> update()));
-
-            timeline.setCycleCount(Timeline.INDEFINITE);
-            timeline.play();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void update() {
-        System.out.println("Running updateWeather method for " + city + "...");
-
-        Coordinates cityCoordinates = GeocodingAPI.getCoordinates(city);
-        System.out.println("cityCoordinates: " + cityCoordinates.toString());
-
-        WeatherData weather = WeatherAPI.getWeather(
-                cityCoordinates.longitude, cityCoordinates.latitude);
-        System.out.println("weather: " + weather.toString());
-
-        titleLabel.setText("Current Weather Statistics for "
-                + weather.getTime() + " at " + city);
-
-        temperatureText.setText(weather.getTemperatureF()
-                + "/" + weather.getTemperature());
-
-        humidityText.setText("" + weather.getRelativeHumidity());
-
-        pressureText.setText("" + weather.getPressure());
     }
 }
---
+++ wsdesktop/src/main/java/io/github/icosadev/wsdesktop/Controller.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2026 LJC
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package io.github.icosadev.wsdesktop;
+
+import io.github.icosadev.wsdesktop.api.Coordinates;
+import io.github.icosadev.wsdesktop.api.GeocodingAPI;
+import io.github.icosadev.wsdesktop.api.WeatherAPI;
+import io.github.icosadev.wsdesktop.api.WeatherData;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
+
+public class Controller {
+        @FXML private Label titleLabel;
+        @FXML private Text temperatureText;
+        @FXML private Text humidityText;
+        @FXML private Text pressureText;
+
+        private static String city = "NYC";
+        private Timeline timeline;
+
+        @FXML
+        public void initialize() {
+                update();
+
+                timeline = new Timeline(
+                                new KeyFrame(Duration.minutes(1), e -> update()));
+                timeline.setCycleCount(Timeline.INDEFINITE);
+                timeline.play();
+        }
+
+        private void update() {
+                System.out.println("Running updateWeather method for " + city + "...");
+
+                Coordinates cityCoordinates = GeocodingAPI.getCoordinates(city);
+                WeatherData weather = WeatherAPI.getWeather(
+                                cityCoordinates.longitude, cityCoordinates.latitude);
+
+                titleLabel.setText("Current Weather Statistics for "
+                                + weather.getTime() + " at " + city);
+
+                temperatureText.setText(weather.getTemperatureF()
+                                + "/" + weather.getTemperature());
+
+                humidityText.setText("" + weather.getRelativeHumidity());
+                pressureText.setText("" + weather.getPressure());
+        }
+}
