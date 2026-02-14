9404fda23cdb808dc9226d414a402a1d571e0b9c
Icosa-Dev <ljcottendev@gmail.com>
2026-02-13 19:06:20 -0500

    Fixed data retreaval in getWeather from WeatherAPI

--- wsdesktop/src/main/java/io/github/icosadev/wsdesktop/api/WeatherAPI.java
+++ wsdesktop/src/main/java/io/github/icosadev/wsdesktop/api/WeatherAPI.java
@@ -11,15 +11,14 @@ import java.net.HttpURLConnection;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-/* NOTE: For API specifications, see:
- * https://open-meteo.com/en/docs#hourly_parameter_definition
- */
-
 public class WeatherAPI extends WebAPI {
+    // TODO: Make this take a Coordinate class input instead of seperate
+    // doubles
     public static WeatherData getWeather(double longitude, double latitude) {
         try {
-            String url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude +
-                    "&longitude=" + longitude + "&current=temperature_2m,relative_humidity_2m,wind_speed_10m";
+            String url = "https://api.open-meteo.com/v1/forecast?latitude=" 
+                + latitude + "&longitude=" + longitude 
+                + "&hourly=temperature_2m&current=temperature_2m,relative_humidity_2m,surface_pressure,wind_speed_10m,wind_direction_10m";
             HttpURLConnection apiConnection = fetchApiResponse(url);
 
             if (apiConnection.getResponseCode() != 200) {
@@ -33,17 +32,20 @@ public class WeatherAPI extends WebAPI {
             JSONObject jsonObject = (JSONObject) parser.parse(jsonResponse);
             JSONObject currentWeatherJson = (JSONObject) jsonObject.get("current");
 
+            String time = (String) currentWeatherJson.get("time");
+
             double temperature = (double) currentWeatherJson.get("temperature_2m");
 
             long relativeHumidity = (long) currentWeatherJson.get("relative_humidity_2m");
 
-            double pressure = (double) currentWeatherJson.get("pressure_msl_surface_pressure");
+            double pressure = (double) currentWeatherJson.get("surface_pressure");
 
             double windSpeed = (double) currentWeatherJson.get("wind_speed_10m");
 
-            double windDirection = (double) currentWeatherJson.get("wind_direction_10m");
+            long windDirection = (long) currentWeatherJson.get("wind_direction_10m");
 
             return new WeatherData(
+                    time, 
                     temperature,
                     relativeHumidity,
                     pressure,
